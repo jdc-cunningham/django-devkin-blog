84254877819aa5d68b9019449ff39df54fd919ad
jcun4128 <jcun4128@gmail.com>
2017-12-01 12:54:49 -0600

    fixed id problem

--- devkin/ver.0.0.1/developing/js/clock.js
+++ devkin/ver.0.0.1/developing/js/clock.js
@@ -2,12 +2,10 @@ let i = 0,
     curSec = new Date().getSeconds(),
     curMsec = parseInt((60 - curSec).toString()+'000');
 
-    console.log(curMsec);
-
 function timedCount() {
     i = i + 1;
     postMessage(i);
-    setTimeout("timedCount()",60000);
+    setTimeout("timedCount()",5000);
 }
 
 setTimeout(timedCount(), curMsec);
